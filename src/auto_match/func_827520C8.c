4f9ee25f8fdf9ba4eade6bec5dbdf2812974e3bc
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-24 08:26:58 -0300

    Match function 0x827520C8

---
+++ src/auto_match/func_827520C8.c
@@ -0,0 +1,6 @@
+void fn_827520C8(int *object, int value, char flag, int other_value)
+{
+    *(int *)(object + 1) = value;
+    *(char *)(object + 2) = flag;
+    *(int *)(object + 3) = other_value;
+}
