fbb2faccc7900042441a1166f4e322fab9e94f0f
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-24 08:32:17 -0300

    Match function 0x82230A08

---
+++ src/auto_match/func_82230A08.c
@@ -0,0 +1,28 @@
+void fn_82230A08(float *param_1, float *param_2)
+{
+    float temp;
+
+    temp = param_1[0];
+    param_1[0] = param_2[0];
+    param_2[0] = temp;
+
+    temp = param_1[1];
+    param_1[1] = param_2[1];
+    param_2[1] = temp;
+
+    temp = param_1[2];
+    param_1[2] = param_2[2];
+    param_2[2] = temp;
+
+    temp = param_1[3];
+    param_1[3] = param_2[3];
+    param_2[3] = temp;
+
+    temp = param_1[4];
+    param_1[4] = param_2[4];
+    param_2[4] = temp;
+
+    temp = param_1[5];
+    param_1[5] = param_2[5];
+    param_2[5] = temp;
+}
