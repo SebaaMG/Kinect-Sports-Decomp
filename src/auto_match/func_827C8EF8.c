21c8a73bb9adfb9dc9b6871c6da5af3e845781d3
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-24 08:26:55 -0300

    Match function 0x827C8EF8

---
+++ src/auto_match/func_827C8EF8.c
@@ -0,0 +1,6 @@
+long long fn_827C8EF8(int param_1, unsigned long long param_2,
+                      unsigned long long param_3, long long param_4)
+{
+    return (unsigned long long)*(unsigned int *)(*(int *)(param_1 + 4) + 0x2c)
+           - param_4;
+}
