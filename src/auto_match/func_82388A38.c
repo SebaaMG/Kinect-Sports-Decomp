b77bdc32b7fbe34c9f057a407a3d815351c25351
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-24 08:03:46 -0300

    Match function 0x82388A38

---
+++ src/auto_match/func_82388A38.c
@@ -0,0 +1,5 @@
+// Kinect Sports retail 4D5308C9, function 0x82388A38 (16 bytes).
+double fn_82388A38(int param_1)
+{
+    return (double)*(float *)(*(int *)(*(int *)(param_1 + 8) + 0x260) + 0x1b8);
+}
