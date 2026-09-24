8b8e3c67cf06315bab1ab0b82c4566e95fa16da0
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-24 07:44:18 -0300

    Match function 0x82247B10

---
+++ src/auto_match/func_82247B10.c
@@ -0,0 +1,6 @@
+extern void fn_8224BF30(void *param_1, void *param_2, unsigned int param_3);
+
+void fn_82247B10(void *param_1, void *param_2, void *param_3)
+{
+    fn_8224BF30(param_2, param_3, 0);
+}
