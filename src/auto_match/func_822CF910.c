6958abd4e5e05fc44f8d4d20643e8a66c4fb1d4f
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-24 06:57:12 -0300

    Match conditional object store at 0x822CF910

---
+++ src/auto_match/func_822CF910.c
@@ -0,0 +1,7 @@
+// Kinect Sports retail 4D5308C9, function 0x822CF910 (16 bytes).
+void fn_822CF910(unsigned int value, void *object)
+{
+    if (object != 0) {
+        *(unsigned int *)((char *)object + 0x164) = value;
+    }
+}
