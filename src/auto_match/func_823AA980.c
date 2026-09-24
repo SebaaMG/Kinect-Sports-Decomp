ce87d022c8e6adf9c7f8597031843009cba04e1d
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-24 08:04:12 -0300

    Match function 0x823AA980

---
+++ src/auto_match/func_823AA980.c
@@ -0,0 +1,5 @@
+// Kinect Sports retail 4D5308C9, function 0x823AA980 (16 bytes).
+void fn_823AA980(int object, float amount)
+{
+    *(float *)(object + 0x58) = *(float *)(object + 0x58) + amount;
+}
