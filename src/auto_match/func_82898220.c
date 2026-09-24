f63136c121ababf768703c26b430cecc0d983e02
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-24 08:30:04 -0300

    Match function 0x82898220

---
+++ src/auto_match/func_82898220.c
@@ -0,0 +1,6 @@
+void fn_82898220(int object, unsigned int value1, unsigned int value2, unsigned int value3)
+{
+    *(unsigned int *)(object + 0x18) = value2;
+    *(unsigned int *)(object + 0x14) = value3;
+    *(unsigned int *)(object + 0x1c) = value1;
+}
