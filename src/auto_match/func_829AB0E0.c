807a607ca67b8d3e3130c04d69a2028aac09499f
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-24 21:00:58 -0300

    Match function 0x829AB0E0

---
+++ src/auto_match/func_829AB0E0.c
@@ -0,0 +1,6 @@
+void fn_829AB0E0(int object, int value1, int value2, int value3)
+{
+    *(int *)(object + 0x548) = value1;
+    *(int *)(object + 0x540) = value2;
+    *(int *)(object + 0x544) = value3;
+}
