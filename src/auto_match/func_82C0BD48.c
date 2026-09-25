40f49cd07d8a2c61c9c57bf4977ffb7a768cf762
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-24 21:03:24 -0300

    Match function 0x82C0BD48

---
+++ src/auto_match/func_82C0BD48.c
@@ -0,0 +1,5 @@
+unsigned long long fn_82C0BD48(unsigned char *param_1)
+{
+    return (unsigned long long) *((unsigned int *) (param_1 + 0x34))
+         - (unsigned long long) *((unsigned int *) (param_1 + 0x44));
+}
