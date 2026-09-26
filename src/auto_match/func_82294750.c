9e0cba36110e7d5dc4ae0c90f1c427537b591fb1
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-26 03:39:37 -0300

    WIP match function 0x82294750 (93.92%)

---
+++ src/auto_match/func_82294750.c
@@ -0,0 +1,9 @@
+extern int lbl_821AA8DC;
+extern int lbl_821AA968;
+void fn_82294750(int param_1)
+{
+    *(int *)(*(int *)(*(int *)(param_1 + -0x50) + 4) + param_1 + -0x50) = &lbl_821AA8DC;
+    fn_822947B8(param_1 + -0x4c);
+    *(int *)(*(int *)(*(int *)(param_1 + -0x50) + 4) + (param_1 + -0x48) + -0x8) = &lbl_821AA968;
+    return;
+}
