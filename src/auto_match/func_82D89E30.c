48ac7b0f16570d1273562e1e734371fc13de5d10
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-25 21:01:38 -0300

    Match function 0x82D89E30

---
+++ src/auto_match/func_82D89E30.c
@@ -0,0 +1,5 @@
+// Kinect Sports retail 4D5308C9, function 0x82D89E30 (16 bytes).
+unsigned int fn_82D89E30(int param_1)
+{
+    return *(unsigned short *)(param_1 + 0xB0) + *(unsigned int *)(param_1 + 0xB8);
+}
