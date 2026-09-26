5c3cc77ad37d673f00fa6843d915226986119a4f
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-26 03:39:38 -0300

    WIP match function 0x823AA498 (91.52%)

---
+++ src/auto_match/func_823AA498.c
@@ -0,0 +1,21 @@
+const float lbl_821CC160;
+
+void fn_823AA498(int object, int param_2, int param_3, int param_4)
+{
+    *(int *)(object + 0x14) = param_4;
+    *(int *)(object + 0x0c) = param_3;
+    *(int *)(object + 0x20) = 0;
+    *(int *)(object + 0x30) = 0;
+    *(float *)(object + 0x58) = lbl_821CC160;
+    *(int *)(object + 0x04) = 1;
+    *(int *)(object + 0x18) = *(int *)(object + 0x18) + 1;
+    *(int *)(object + 0x34) = 0;
+    *(int *)(object + 0x38) = 0;
+    *(int *)(object + 0x3c) = 8;
+    *(int *)(object + 0x40) = 0;
+    *(int *)(object + 0x44) = 8;
+    *(int *)(object + 0x48) = 0;
+    *(int *)(object + 0x4c) = 8;
+    *(int *)(object + 0x50) = 0;
+    *(int *)(object + 0x54) = 9;
+}
