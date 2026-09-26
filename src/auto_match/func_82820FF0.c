98e7e6f2ac0c179943930fd528864ba2e87828ba
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-26 14:53:58 -0300

    WIP match function 0x82820FF0 (96.79%)

--- src/auto_match/func_82820FF0.c
+++ src/auto_match/func_82820FF0.c
@@ -1,16 +1,13 @@
-void fn_82820FF0(unsigned int *param_1)
+void fn_82820FF0(volatile unsigned int *param_1)
 {
     unsigned int uVar1;
     
     uVar1 = param_1[0];
-    param_1[0] = (uVar1 >> 16 | uVar1 & 0xFFFF0000) >> 8 & 0xFFFF |
-                 ((uVar1 << 16 | uVar1 & 0xFFFF) & 0xFFFF00) << 8;
+    param_1[0] = ((uVar1 >> 16 | uVar1 & 0xFFFF0000) >> 8 & 0xFFFF) | (((uVar1 << 16 | uVar1 & 0xFFFF) & 0xFFFF00) << 8);
     
     uVar1 = param_1[1];
-    param_1[1] = (uVar1 >> 16 | uVar1 & 0xFFFF0000) >> 8 & 0xFFFF |
-                 ((uVar1 << 16 | uVar1 & 0xFFFF) & 0xFFFF00) << 8;
+    param_1[1] = ((uVar1 >> 16 | uVar1 & 0xFFFF0000) >> 8 & 0xFFFF) | (((uVar1 << 16 | uVar1 & 0xFFFF) & 0xFFFF00) << 8);
     
     uVar1 = param_1[2];
-    param_1[2] = (uVar1 >> 16 | uVar1 & 0xFFFF0000) >> 8 & 0xFFFF |
-                 ((uVar1 << 16 | uVar1 & 0xFFFF) & 0xFFFF00) << 8;
+    param_1[2] = ((uVar1 >> 16 | uVar1 & 0xFFFF0000) >> 8 & 0xFFFF) | (((uVar1 << 16 | uVar1 & 0xFFFF) & 0xFFFF00) << 8);
 }
