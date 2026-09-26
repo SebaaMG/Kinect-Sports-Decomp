a3bf9bd1d8379462e4e8f9d1cdd5c7efdff3f187
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-25 21:01:35 -0300

    Match function 0x82F59478

---
+++ src/auto_match/func_82F59478.c
@@ -0,0 +1,7 @@
+// Kinect Sports retail 4D5308C9, function 0x82F59478 (16 bytes).
+void fn_82F59478(float *param_1, float param_2, float param_3, float param_4)
+{
+    *param_1 = param_2;
+    param_1[1] = param_3;
+    param_1[2] = param_4;
+}
