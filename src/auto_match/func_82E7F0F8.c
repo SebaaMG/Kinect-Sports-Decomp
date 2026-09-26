ce000fea802b014f900976d8999a3328d7dd1e65
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-25 21:01:11 -0300

    Match function 0x82E7F0F8

---
+++ src/auto_match/func_82E7F0F8.c
@@ -0,0 +1,8 @@
+// Kinect Sports retail 4D5308C9, function 0x82E7F0F8 (16 bytes).
+typedef unsigned long long undefined8;
+
+void fn_82E7F0F8(undefined8 *param_1)
+{
+    *param_1 = 0;
+    param_1[1] = 0;
+}
