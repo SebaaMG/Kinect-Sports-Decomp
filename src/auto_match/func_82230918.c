f4054359dae87f260d307a74a08180c82cbdd257
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-26 03:39:37 -0300

    WIP match function 0x82230918 (97.50%)

---
+++ src/auto_match/func_82230918.c
@@ -0,0 +1,7 @@
+#pragma comment(linker, "/alternatename:??_7GAcquireInterface@@6B@=GAcquireInterface_vftable")
+extern const void *GAcquireInterface_vftable;
+
+void fn_82230918(void *object)
+{
+    *(unsigned int *)object = (unsigned int)&GAcquireInterface_vftable;
+}
