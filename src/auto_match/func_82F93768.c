1ab9056e577f9b3cf4551a677f0a8bef1d18c20c
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-26 03:39:37 -0300

    WIP match function 0x82F93768 (99.25%)

---
+++ src/auto_match/func_82F93768.c
@@ -0,0 +1,7 @@
+// Kinect Sports retail 4D5308C9, function 0x82F93768 (16 bytes).
+int fn_82F93768(int param_1, char param_2)
+{
+    volatile int local_1 = param_1;
+    volatile char local_2 = param_2;
+    return local_1;
+}
