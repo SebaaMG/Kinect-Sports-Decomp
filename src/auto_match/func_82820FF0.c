1737a1fd6edf0ca765e8040474545e5fe905ed34
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-26 03:06:09 -0300

    WIP match function 0x82820FF0 (0.00%)

---
+++ src/auto_match/func_82820FF0.c
@@ -0,0 +1,16 @@
+void fn_82820FF0(unsigned int *param_1)
+{
+    unsigned int uVar1;
+    
+    uVar1 = param_1[0];
+    param_1[0] = (uVar1 >> 16 | uVar1 & 0xFFFF0000) >> 8 & 0xFFFF |
+                 ((uVar1 << 16 | uVar1 & 0xFFFF) & 0xFFFF00) << 8;
+    
+    uVar1 = param_1[1];
+    param_1[1] = (uVar1 >> 16 | uVar1 & 0xFFFF0000) >> 8 & 0xFFFF |
+                 ((uVar1 << 16 | uVar1 & 0xFFFF) & 0xFFFF00) << 8;
+    
+    uVar1 = param_1[2];
+    param_1[2] = (uVar1 >> 16 | uVar1 & 0xFFFF0000) >> 8 & 0xFFFF |
+                 ((uVar1 << 16 | uVar1 & 0xFFFF) & 0xFFFF00) << 8;
+}
