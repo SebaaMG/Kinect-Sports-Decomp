a2005ac562207a8c7db7ef2575283ae34ac62897
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-26 03:39:37 -0300

    WIP match function 0x8223B600 (97.50%)

---
+++ src/auto_match/func_8223B600.c
@@ -0,0 +1,8 @@
+#pragma comment(linker, "/alternatename:PTR_Function_82F63EF0_82002b24=??_7_Ref_count_base@tr1@std@@6B@")
+
+extern unsigned int PTR_Function_82F63EF0_82002b24;
+
+void fn_8223B600(unsigned int *param_1)
+{
+    *param_1 = (unsigned int)&PTR_Function_82F63EF0_82002b24;
+}
