ee66b7511b92f8ae78d9f0717a1fc7ff2c6c7fab
SebaaMG <166541446+SebaaMG@users.noreply.github.com>
2026-09-25 21:01:32 -0300

    Match function 0x82D585C0

---
+++ src/auto_match/func_82D585C0.c
@@ -0,0 +1,6 @@
+// Kinect Sports retail 4D5308C9, function 0x82D585C0 (16 bytes).
+unsigned int fn_82D585C0(int param_1, unsigned char *param_2, unsigned int param_3)
+{
+    *param_2 = 0;
+    return param_3;
+}
